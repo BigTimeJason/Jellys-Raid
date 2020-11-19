b8260d4b8c518e65b6dc6a9f39eadddee8e5d260
BigTimeJason <49235257+BigTimeJason@users.noreply.github.com>
2020-11-19 12:56:19 +0000

    new containers

---
+++ src/ContentContainer.js
@@ -0,0 +1,17 @@
+class ContentContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+            type: 0,
+            content: "",
+        }
+    }
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default ContentContainer;
---
+++ src/TextContainer.js
@@ -0,0 +1,15 @@
+class TextContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+        }
+    }
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default TextContainer;
